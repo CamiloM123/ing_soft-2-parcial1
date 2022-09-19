bdfdbd6a6f5e060810660d2d327a8ef2a316c2fe
CamiloM123 <juanc.munozv@autonoma.edu.co>
2022-09-19 17:54:58 -0500

    Add purchase model and route - try the http request

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express()
 const port = 5000 || process.env.PORT;
+const purchaseRoute = require("./routes/purchase.routes")
 require('dotenv').config()
 
 //Port connection
@@ -9,8 +10,15 @@ app.listen(port, () => {
     console.log("Running in the port", port);
   });
 
-app.get('/',(req,res)=>res.send("Parcial-1"))
-  
+//Middleware
+app.use(express.json())
+app.use('/api', purchaseRoute)
+
+//Project routes
+app.get("/", (req, res) => {
+    res.send("Welcome to my API");
+  });
+
 mongoose
     .connect(process.env.CONNECTION_STRING_MONGODB)
     .then(() => {
