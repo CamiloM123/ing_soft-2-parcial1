9dfd3e7b9bb940476b6d724e74b812bdd84b84fa
CamiloM123 <juanc.munozv@autonoma.edu.co>
2022-09-19 16:43:26 -0500

    Add mongoDB connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express()
 const port = 5000 || process.env.PORT;
-
+require('dotenv').config()
 
 //Port connection
 app.listen(port, () => {
     console.log("Running in the port", port);
   });
 
+app.get('/',(req,res)=>res.send("Parcial-1"))
+  
+mongoose
+    .connect(process.env.CONNECTION_STRING_MONGODB)
+    .then(() => {
+      console.log("Successful connection with MongoDB");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  
+
+  
